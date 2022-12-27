5ec1f8b68c879c0cc41844cc25579b45b2db086c
ajitkumar2804 <ajitjagtap67@gmail.com>
2022-12-27 12:47:31 +0530

    useEffect warning removed

--- src/Components/categories.js
+++ src/Components/categories.js
@@ -5,15 +5,16 @@ export default function Categoriees(props) {
     const [news, setNews] = useState([]);
     const [loaded, setLoaded] = useState(false);
 
-    const getNews = async () => {
-        await axios.get(`https://newsapi.org/v2/top-headlines/sources?category=${props.cat}&apiKey=f67f2c7afa394e38a83ffdcd6d7bb7a0`)
-            .then(res => res.data)
-            .then(data => setNews(data))
-        setLoaded(true)
-    }
+    
 
     useEffect(() => {
-        getNews();
+        const getNews = async () => {
+            await axios.get(`https://newsapi.org/v2/top-headlines/sources?category=${props.cat}&apiKey=f67f2c7afa394e38a83ffdcd6d7bb7a0`)
+                .then(res => res.data)
+                .then(data => setNews(data))
+            setLoaded(true)
+        }
+        getNews()
     },[props.cat])
 
     if (loaded) {
